672aac92aa38251e24a12ff353783f745bf81ca4
Mithu <cemithu06@gmail.com>
2024-11-24 18:22:04 +0600

    React and uploader

--- browser-sync.js
+++ browser-sync.js
@@ -4,5 +4,5 @@ const browserSync = require('browser-sync').create();
 browserSync.init({
 	proxy: "http://localhost:3000", // Your Node.js app URL
 	files: ["public/**/*.*"],      // Watch files in the 'public' directory
-	port: 3000                     // BrowserSync runs on port 4000
+	port: 4000                     // BrowserSync runs on port 4000
 });
